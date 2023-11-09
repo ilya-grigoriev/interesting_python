6b47c2513d54bbb68ec45a9004280b70ae8e7e1f
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-11-10 01:34:37 +0300

    feat(`gen_or_der`): added new functional

--- gen_or_def.py
+++ gen_or_def.py
@@ -12,3 +12,8 @@ print(func([2]))   # print that it is generator
 
 for i in func([2]):
     print(i)   # we don't enter in here
+
+try:
+    next(func([2]))
+except StopIteration as e:
+    print(e.value)   # print 1
